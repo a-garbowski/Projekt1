8ea5a311945864cedc649ba09801e8327da8754f
a-garbowski <nornica2907@gmail.com>
2024-03-05 19:41:22 +0100

    Initial commit

---
+++ app/src/main/java/artur/projekt1/model/FormModel.kt
@@ -0,0 +1,24 @@
+package artur.projekt1.model
+
+enum class SubscriptionType {
+    Free, Premium
+}
+
+data class FormModel(
+    val email: String,
+    val password: String,
+    val accType: SubscriptionType,
+    val marketingConsent: Boolean,
+    val newsletterConsent: Boolean,
+) {
+    override fun toString(): String {
+        val displayedPassword = "".padStart(password.length, '*')
+
+        return StringBuilder("")
+            .append("E-mail: ").append(email).append("\n")
+            .append("Password: ").append(displayedPassword).append("\n")
+            .append("Subscription type: ").append(accType).append("\n")
+            .append("Newsletter enabled: ").append(newsletterConsent)
+            .toString()
+    }
+}
